3f95d3dae4fe8b545b0e662d6ee2b333b9b3f687
msk53 <129752120+msk53@users.noreply.github.com>
2023-07-27 01:23:18 +0900

    [MiniAxe]USBハブ付きテンキーでバスパワー不測の警告が出ないようにした - USB_MAX_POWER_CONSUMPTION を デフォルト値500mA -> 100mA に変更

--- keyboards/miniaxe/keymaps/msk53/config.h
+++ keyboards/miniaxe/keymaps/msk53/config.h
@@ -9,3 +9,8 @@
 // https://github.com/qmk/qmk_firmware/blob/master/docs/ja/keycodes_us_ansi_shifted.md
 #define USB_POLLING_INTERVAL_MS 10
 
+// 安物のUSBハブ付きテンキーでバスパワー不測の警告が出たので、
+// デフォルト値500mA -> 100mA に変更
+// ※実測はしていないが、LED付き75%キーボードのMD770が警告なしで動いているのだから、
+// 　LEDなし36キーのMiniAxeが動かないわけがない
+#define USB_MAX_POWER_CONSUMPTION 100
