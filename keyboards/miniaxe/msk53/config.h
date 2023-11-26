2178f111e32c2ab3bd00d52f32fa41741efb81a4
msk53 <129752120+msk53@users.noreply.github.com>
2023-11-26 20:06:09 +0900

    [MiniAxe][msk53]レイヤー数の指定をキーマップに移動

--- keyboards/miniaxe/msk53/config.h
+++ keyboards/miniaxe/msk53/config.h
@@ -4,11 +4,6 @@
 #undef VENDOR_ID
 #define VENDOR_ID 0xBEEF
 
-// VIAのレイヤー数にNICOLA用レイヤー分を追加
-#define DYNAMIC_KEYMAP_LAYER_COUNT 5
-// 8枚以上レイヤーを使うことはないので削減
-#define LAYER_STATE_8BIT
-
 // WindowsのリモートデスクトップにShiftが送られていないので、
 // 1000Hz -> 100Hz に戻す
 // https://github.com/qmk/qmk_firmware/blob/master/docs/ja/keycodes_us_ansi_shifted.md
--- keyboards/miniaxe/msk53/keymaps/nicola/config.h
+++ keyboards/miniaxe/msk53/keymaps/nicola/config.h
@@ -1,4 +1,7 @@
 #pragma once
 
+// 基本4レイヤー + NICOLA用レイヤー
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+
 // MiniAxeは36キーなので、Pキーで読点「、」を入力する
 #define NICOLA_FUZZY_PUNCTUATION
