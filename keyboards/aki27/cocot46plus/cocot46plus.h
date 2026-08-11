775f8ec8243e2b9e170e39c125cfa6372b9348be
msk53 <129752120+msk53@users.noreply.github.com>
2026-08-11 18:28:19 +0900

    [cocot46plus]cocot_keycodes.COCOT_SAFE_RANGEを参照することで残りのQK_KB_nを使用できるように変更した ・difineでのQK_KB_n定義をcocot_keycodesに統合した ・COCOT_SAFE_RANGEを、SAFE_RANGE(QK_USER_0)からcocot_keycodesの終端に変更した

--- keyboards/aki27/cocot46plus/cocot46plus.h
+++ keyboards/aki27/cocot46plus/cocot46plus.h
@@ -32,24 +32,17 @@ typedef union {
 } cocot_config_t;
 
 enum cocot_keycodes {
-
-    COCOT_SAFE_RANGE = SAFE_RANGE,
-    CPI_SW,
+    CPI_SW = QK_KB_0,
     SCRL_SW,
     ROT_R15,
     ROT_L15,
     SCRL_MO,
     SCRL_TO,
-    SCRL_IN
+    SCRL_IN,
+
+    COCOT_SAFE_RANGE
 };
 
-#define CPI_SW QK_KB_0
-#define SCRL_SW QK_KB_1
-#define ROT_R15 QK_KB_2
-#define ROT_L15 QK_KB_3
-#define SCRL_MO QK_KB_4
-#define SCRL_TO QK_KB_5
-#define SCRL_IN QK_KB_6
 
 extern cocot_config_t cocot_config;
 
