6c111cb3c8cab62a340d1ce2ba4588eb8982d7ee
msk53 <129752120+msk53@users.noreply.github.com>
2026-08-22 16:23:16 +0900

    [cocot46plus][msk53]NICOLA配列をベースに自分用キーマップを追加

---
+++ keyboards/aki27/cocot46plus/keymaps/msk53/config.h
@@ -0,0 +1,14 @@
+#pragma once
+
+// 親指シフト配列: Pキーで読点「、」を入力する
+#define NICOLA_FUZZY_PUNCTUATION
+
+// 容量削減
+#define LAYER_STATE_8BIT
+#define NO_MUSIC_MODE
+
+// OLEDに表示するレイヤーの文字列を上書き
+#define OLED_STRING_LAYER_1 "NICOLA"
+#define OLED_STRING_LAYER_2 " Lower"
+#define OLED_STRING_LAYER_3 " Raise"
+#define OLED_STRING_LAYER_4 " Mouse"
---
+++ keyboards/aki27/cocot46plus/keymaps/msk53/rules.mk
@@ -0,0 +1,12 @@
+VIA_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
+
+# 注意: 文末コメントを入れると下記の通りエラーになる
+#	make[1]: *** users/nicola: Is a directory.  中止.
+USER_NAME := nicola
+
+# 容量削減
+GRAVE_ESC_ENABLE = no
+MUSIC_ENABLE = no
+
+
